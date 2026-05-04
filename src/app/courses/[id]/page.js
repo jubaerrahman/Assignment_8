09a70b11de19489d01607363098a1d31d269c904
Jubaer Rahman <coursesmy3@gmail.com>
2026-05-04 06:27:10 +0600

    fixed

--- src/app/courses/[id]/page.js
+++ src/app/courses/[id]/page.js
@@ -1,11 +1,13 @@
-export const dynamic = "force-dynamic"
-export const revalidate = 0
-
 import DetailsClient from "./DetailsClient"
+import {courses} from "@/data/courses"
+
+export async function generateStaticParams(){
+  return courses.map(c => ({
+    id: String(c.id)
+  }))
+}
 
 export default async function Page({ params }) {
-
-const { id } = await params
-
-return <DetailsClient id={id} />
+  const { id } = await params
+  return <DetailsClient id={id} />
 }
