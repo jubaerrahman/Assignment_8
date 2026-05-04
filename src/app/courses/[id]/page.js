6474ce9fe08526507935cfb95a3e01634e574fa8
Jubaer Rahman <coursesmy3@gmail.com>
2026-05-04 06:22:22 +0600

    fixed

--- src/app/courses/[id]/page.js
+++ src/app/courses/[id]/page.js
@@ -1,9 +1,18 @@
 export const dynamic = "force-dynamic"
+
+import {courses} from "@/data/courses"
 import DetailsClient from "./DetailsClient"
+import { notFound } from "next/navigation"
 
 export default async function Page({ params }) {
 
-const { id } = await params   // 👈 FIX HERE
+const { id } = await params
+
+const course = courses.find(c => Number(c.id) === Number(id))
+
+if(!course){
+  notFound()   // ✅ SERVER SAFE
+}
 
 return <DetailsClient id={id} />
 }
